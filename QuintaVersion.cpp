c22ecb776a2cd15ab4bd4fc8c739c50592a47851
LapegelagartaBriones <juchbriones@gmail.com>
2022-11-30 18:55:21 -0600

    En la quinta version empece a desarrollar mi caso 1 con un for para introducir el coeficiente del polinomio

---
+++ QuintaVersion.cpp
@@ -0,0 +1,21 @@
+//En la quinta version empece a desarrollar mi caso 1 con un for para introducir el coeficiente del polinomio
+#include<stdio.h>
+
+int main() {
+	int eleccion;
+	float polinomio[12];
+	printf("\n\tCalculadora de polinomios\n\n");
+	printf("\n\tEn el siguiente menu elija la opcion que quiera:\n\n");
+	printf("\n\t1- Introducir el polinomio y evaluarlo\n");
+	printf("\n\t2- Multiplicar 2 polinomios\n");
+	printf("\n\t3- Dividir 2 polinomios\n");
+	printf("\n\teleccion: ");
+	scanf_s("%i", &eleccion);
+	switch (eleccion) {
+	case 1:for (int i = 0; i <= 12; i++)
+	{
+		printf("Introduzca el coeficiente del polinomio: x^%i\n", i);
+		scanf_s("%f", &polinomio[i]);
+	}
+	}
+}
